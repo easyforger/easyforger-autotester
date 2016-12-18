e1054067d0be8adcd6ac20c500f6f107cbfaf026
JCranky <paulo.siqueira@gmail.com>
2016-12-18 14:16:04 +0100

    Follow up to easyforger #60
    Also adds new tests.

--- src/main/scala/com/easyforger/recipes/test/RecipeSupportSpec.scala
+++ src/main/scala/com/easyforger/recipes/test/RecipeSupportSpec.scala
@@ -4,7 +4,6 @@
  */
 package com.easyforger.recipes.test
 
-import com.easyforger.base.EasyForger
 import com.easyforger.recipes.RecipeSupport
 import net.minecraft.block.Block
 import net.minecraft.init.{Blocks, Items}
@@ -12,37 +11,39 @@ import net.minecraft.item.Item
 import org.specs2.matcher.Matcher
 import org.specs2.mutable.Specification
 
-class RecipeSupportSpec extends Specification with EasyForger {
+class RecipeSupportSpec extends Specification {
+  import com.easyforger.autotester.Specs2RunnerMod._ // scalastyle:ignore
+
   "RecipeSupport.shortForItemStack" should {
     "get the short for Items" in {
-      "'a' for apple" >> (Items.apple must haveItemShort('a'))
-      "'i' for iron_axe" >> (Items.iron_axe must haveItemShort('i'))
-      "'s' for sign" >> (Items.sign must haveItemShort('s'))
-      "'s' for wheat_seeds" >> (Items.wheat_seeds must haveItemShort('s'))
-      "'a' for armor_stand" >> (Items.armor_stand must haveItemShort('a'))
-      "'c' for item cake" >> (Items.cake must haveItemShort('c'))
-      "'a' for item acacia_door" >> (Items.acacia_door must haveItemShort('a'))
-      "'j' for item jungle_door" >> (Items.jungle_door must haveItemShort('j'))
-      "'b' for item bed" >> (Items.bed must haveItemShort('b'))
+      "'a' for apple" >> (Items.APPLE must haveItemShort('a'))
+      "'i' for iron_axe" >> (Items.IRON_AXE must haveItemShort('i'))
+      "'s' for sign" >> (Items.SIGN must haveItemShort('s'))
+      "'s' for wheat_seeds" >> (Items.WHEAT_SEEDS must haveItemShort('s'))
+      "'a' for armor_stand" >> (Items.ARMOR_STAND must haveItemShort('a'))
+      "'c' for item cake" >> (Items.CAKE must haveItemShort('c'))
+      "'a' for item acacia_door" >> (Items.ACACIA_DOOR must haveItemShort('a'))
+      "'j' for item jungle_door" >> (Items.JUNGLE_DOOR must haveItemShort('j'))
+      "'b' for item bed" >> (Items.BED must haveItemShort('b'))
     }
 
     "get the short for Blocks" in {
-      "'d' for dirt" >> (Blocks.dirt must haveBlockShort('d'))
-      "'f' for furnace" >> (Blocks.furnace must haveBlockShort('f'))
-      "'g' for grass" >> (Blocks.grass must haveBlockShort('g'))
-      "'b' for brewing_stand" >> (Blocks.brewing_stand must haveBlockShort('b'))
-      "'r' for reeds" >> (Blocks.reeds must haveBlockShort('r'))
-      "'a' for acacia_fence" >> (Blocks.acacia_fence must haveBlockShort('a'))
-      "'a' for acacia_fence_gate" >> (Blocks.acacia_fence_gate must haveBlockShort('a'))
-      "'a' for acacia_stairs" >> (Blocks.acacia_stairs must haveBlockShort('a'))
-      "'b' for birch_stairs" >> (Blocks.birch_stairs must haveBlockShort('b'))
-      "'c' for block cake" >> (Blocks.cake must haveBlockShort('c'))
-      "'a' for block acacia_door" >> (Blocks.acacia_door must haveBlockShort('a'))
-      "'j' for block jungle_door" >> (Blocks.jungle_door must haveBlockShort('j'))
-      "'b' for block birch_door" >> (Blocks.birch_door must haveBlockShort('b'))
-      "'o' for block oak_door" >> (Blocks.oak_door must haveBlockShort('o'))
-      "'d' for block dark_oak_door" >> (Blocks.dark_oak_door must haveBlockShort('d'))
-      "'b' for block bed" >> (Blocks.bed must haveBlockShort('b'))
+      "'d' for dirt" >> (Blocks.DIRT must haveBlockShort('d'))
+      "'f' for furnace" >> (Blocks.FURNACE must haveBlockShort('f'))
+      "'g' for grass" >> (Blocks.GRASS must haveBlockShort('g'))
+      "'b' for brewing_stand" >> (Blocks.BREWING_STAND must haveBlockShort('b'))
+      "'r' for reeds" >> (Blocks.REEDS must haveBlockShort('r'))
+      "'a' for acacia_fence" >> (Blocks.ACACIA_FENCE must haveBlockShort('a'))
+      "'a' for acacia_fence_gate" >> (Blocks.ACACIA_FENCE_GATE must haveBlockShort('a'))
+      "'a' for acacia_stairs" >> (Blocks.ACACIA_STAIRS must haveBlockShort('a'))
+      "'b' for birch_stairs" >> (Blocks.BIRCH_STAIRS must haveBlockShort('b'))
+      "'c' for block cake" >> (Blocks.CAKE must haveBlockShort('c'))
+      "'a' for block acacia_door" >> (Blocks.ACACIA_DOOR must haveBlockShort('a'))
+      "'j' for block jungle_door" >> (Blocks.JUNGLE_DOOR must haveBlockShort('j'))
+      "'b' for block birch_door" >> (Blocks.BIRCH_DOOR must haveBlockShort('b'))
+      "'o' for block oak_door" >> (Blocks.OAK_DOOR must haveBlockShort('o'))
+      "'d' for block dark_oak_door" >> (Blocks.DARK_OAK_DOOR must haveBlockShort('d'))
+      "'b' for block bed" >> (Blocks.BED must haveBlockShort('b'))
     }
 
     "get the short for special items" in {
--- src/main/scala/com/easyforger/recipes/test/ShapedRecipesSpec.scala
+++ src/main/scala/com/easyforger/recipes/test/ShapedRecipesSpec.scala
@@ -4,13 +4,15 @@
  */
 package com.easyforger.recipes.test
 
-import com.easyforger.base.EasyForger
+import com.easyforger.recipes.RecipeSupport
 import net.minecraft.init.Items
 import net.minecraft.item.ItemStack
 import org.specs2.matcher.Matcher
 import org.specs2.mutable.Specification
 
-class ShapedRecipesSpec extends Specification with EasyForger {
+class ShapedRecipesSpec extends Specification {
+  import com.easyforger.autotester.Specs2RunnerMod._ // scalastyle:ignore
+
   sequential
   isolated
 
@@ -20,56 +22,56 @@ class ShapedRecipesSpec extends Specification with EasyForger {
 
   "a dsl shaped recipe" should {
     "have the components for the saddle" in {
-      val recipe = (Items.leather + Items.iron_ingot) to Items.saddle withShape
+      val recipe = (Items.LEATHER + Items.IRON_INGOT) to Items.SADDLE withShape
         """
           |...
           |lll
           |i.i
         """.stripMargin
 
-      val params = calcParamsArrays(recipe)
+      val params = RecipeSupport.calcMCParamsArray(recipe)
       params(0) === "   "
       params(1) === "lll"
       params(2) === "i i"
       params(3) === 'l'
-      params(4).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.leather))
+      params(4).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.LEATHER))
       params(5) === 'i'
-      params(6).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.iron_ingot))
+      params(6).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.IRON_INGOT))
     }
 
     "have the components for diamond + carrots to armor stand" in {
-      val recipe = Items.diamond + Items.carrot to Items.armor_stand withShape
+      val recipe = Items.DIAMOND + Items.CARROT to Items.ARMOR_STAND withShape
         """
           |.c.
           |.d.
           |.d.
         """.stripMargin
 
-      val params = calcParamsArrays(recipe)
+      val params = RecipeSupport.calcMCParamsArray(recipe)
       params(0) === " c "
       params(1) === " d " // scalastyle:ignore
       params(2) === " d "
       params(3) === 'd'
-      params(4).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.diamond))
+      params(4).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.DIAMOND))
       params(5) === 'c'
-      params(6).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.carrot))
+      params(6).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.CARROT))
     }
 
     "have the components for iron_ingot + redDye to apple" in {
-      val recipe = Items.iron_ingot + redDye to Items.apple withShape
+      val recipe = Items.IRON_INGOT + redDye to Items.APPLE withShape
         """
           |.i.
           |.i.
           |.r.
         """.stripMargin
 
-      val params = calcParamsArrays(recipe)
+      val params = RecipeSupport.calcMCParamsArray(recipe)
 
       params(0) === " i " // scalastyle:ignore
       params(1) === " i "
       params(2) === " r "
       params(3) === 'i'
-      params(4).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.iron_ingot))
+      params(4).asInstanceOf[ItemStack] must beSameStackAs(new ItemStack(Items.IRON_INGOT))
       params(5) === 'r'
       params(6).asInstanceOf[ItemStack] must beSameStackAs(redDye)
     }
